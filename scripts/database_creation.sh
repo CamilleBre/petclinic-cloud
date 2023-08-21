2604c12aa4beadde7c688be6097d15b82b2368bb
CamilleBre <camille.breuil.pro@gmail.com>
2023-08-21 15:00:33 +0200

    change namespace in db creation script

--- scripts/database_creation.sh
+++ scripts/database_creation.sh
@@ -1,5 +1,5 @@
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
-helm install vets-db-mysql bitnami/mysql --namespace spring-petclinic  --set auth.database=service_instance_db --set primary.persistence.enabled=false --set secondary.persistence.enabled=false
-helm install visits-db-mysql bitnami/mysql --namespace spring-petclinic   --set auth.database=service_instance_db --set primary.persistence.enabled=false --set secondary.persistence.enabled=false
-helm install customers-db-mysql bitnami/mysql --namespace spring-petclinic   --set auth.database=service_instance_db --set primary.persistence.enabled=false --set secondary.persistence.enabled=false
+helm install vets-db-mysql bitnami/mysql --namespace dev  --set auth.database=service_instance_db --set primary.persistence.enabled=false --set secondary.persistence.enabled=false
+helm install visits-db-mysql bitnami/mysql --namespace dev   --set auth.database=service_instance_db --set primary.persistence.enabled=false --set secondary.persistence.enabled=false
+helm install customers-db-mysql bitnami/mysql --namespace dev   --set auth.database=service_instance_db --set primary.persistence.enabled=false --set secondary.persistence.enabled=false
